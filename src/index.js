f8fb625b600bf88f934a6fb2d97f22d4e4ef1590
Melvin <melvin@ylly.fr>
2020-04-25 13:02:29 +0200

    ref #2 :
    * Create Router
    * Create first route

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
 import './index.css'
+import Router from './Router'
 import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router />
   </React.StrictMode>,
   document.getElementById('root')
 )
