6dcdbc6478b92070b6b81080b96fa183ae1a1980
Melvin Cado <melvin.cado@gmail.com>
2021-03-23 19:44:56 +0100

    Deployment

---
+++ build/precache-manifest.440b76c648cb93dee8ff71207b7edc0a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea23e5c86575bd235818af15932384d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0cd685ade6d9e5c1fdf",
+    "url": "/static/css/2.4a533915.chunk.css"
+  },
+  {
+    "revision": "82c9d15df3b7e7de5a4b",
+    "url": "/static/css/main.20827ce3.chunk.css"
+  },
+  {
+    "revision": "e0cd685ade6d9e5c1fdf",
+    "url": "/static/js/2.5416774b.chunk.js"
+  },
+  {
+    "revision": "1470dc37e47a7ab8668b607966104744",
+    "url": "/static/js/2.5416774b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82c9d15df3b7e7de5a4b",
+    "url": "/static/js/main.97e709a4.chunk.js"
+  },
+  {
+    "revision": "7020efe1eb34d36aa8f5",
+    "url": "/static/js/runtime-main.1b509dfe.js"
+  },
+  {
+    "revision": "99152337ae781543cdad9ee75d08b76d",
+    "url": "/static/media/power-off-solid-dark-blue3.99152337.svg"
+  }
+]);
--- src/config/parameters.js
+++ src/config/parameters.js
@@ -1,4 +1,7 @@
-export const api = 'http://localhost:3033'
+const env = 'prod'
+
+export const api =
+  env === 'dev' ? 'http://localhost:3033' : 'http://api.family-saves.com'
 
 export const configApi = (token = null) => {
   if (token !== null) {
